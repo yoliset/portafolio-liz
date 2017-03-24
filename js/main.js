307d521eedb0be96e93f85f76267725d349685f9
yolisett <lizbeth9589@gmail.com>
2017-03-24 02:16:06 -0500

    Arreglo en css y html

---
+++ js/main.js
@@ -0,0 +1,24 @@
+ // Array con las imagenes que se iran mostrando en la web
+
+    var imagenes=new Array(
+        'img/imagen/1.jpg'
+        
+    );
+    //Funcion para cambiar la imagen
+    function rotarImagenes()
+    {
+        // obtenemos un numero aleatorio entre 0 y la cantidad de imagenes que hay
+        var index=Math.floor((Math.random()*imagenes.length));
+ 
+        // cambiamos la imagen
+        document.getElementById("imagen").src=imagenes[index];
+    }
+    //Función que se ejecuta una vez cargada la página
+    onload=function()
+    {
+        // Cargamos una imagen aleatoria
+        rotarImagenes();
+ 
+        // Indicamos que cada 2 segundos cambie la imagen
+        setInterval(rotarImagenes,2000);
+    }
